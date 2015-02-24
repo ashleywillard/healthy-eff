b5c345c720cce379c50548c7eb6fea6242c7545a
mijelin <mj.lin@berkeley.edu>
2015-02-24 13:43:17 -0400

    Initial commit (new/clean rails app)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Healthyeff::Application.config.secret_token = 'cc5b0f176dae81410723589a5565e5257dac68769125df6c52c7b85af7303d05d79f2b92f85ab16dd632a55a6aea180c98717d5f7e1efe2ef3e2505dcc912672'
