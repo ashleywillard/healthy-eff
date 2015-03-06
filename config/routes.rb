f093504d4e252794bc718035cdc4b1460cf93044
Allan Tang (saasbook) <allan_tang@berkeley.edu>
2015-03-06 13:14:42 -0800

    Testing another push, changing saasbook username

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Healthyeff::Application.routes.draw do
 
-  #added by devise
+  #added by devise -allan
   devise_for :users
 
   #required for devise to work, redirects to path below after user logs in
-  #change home#index to whatever need be
+  #change home#index to whatever need be -allan
   root to: "home#index"
 
   # The priority is based upon order of creation:
