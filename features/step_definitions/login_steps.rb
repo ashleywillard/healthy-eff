6e00a1bb937876c4a4a3d0ee3245b6e21c2a7ecb
Allan Tang (saasbook) <allan_tang@berkeley.edu>
2015-03-08 20:52:42 -0700

    Saving local changes

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -1,10 +1,22 @@
 Given /the following users exist/ do |users_table|
-	users_table.hashes.each do |user|
-		User.create!(user)
-	end
+  users_table.hashes.each do |user|
+    User.create!(user)
+  end
 end
 
-Given /I am signed in/
-
+Given /I am signed in/ do
+  visit '/users/sign_up'
+  fill_in "email", :with => "169.healthyeff@gmail.com"
+  fill_in "password", :with => "northsidepotato"
+  fill_in "password", :with => "northsidepotato"
+  click_button "Sign up"
 end
+
+And /I am on any page/ do
+	# Undefined for now 
+end
+
+
+
+
 
