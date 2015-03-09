cac34cfe511574a7674c907c1446b841d042da7c
Allan Tang (saasbook) <allan_tang@berkeley.edu>
2015-03-08 21:12:35 -0700

    Still working on cucumber

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -5,11 +5,10 @@ Given /the following users exist/ do |users_table|
 end
 
 Given /I am signed in/ do
-  visit '/users/sign_up'
-  fill_in "email", :with => "169.healthyeff@gmail.com"
-  fill_in "password", :with => "northsidepotato"
-  fill_in "password", :with => "northsidepotato"
-  click_button "Sign up"
+  visit '/users/sign_in'
+  fill_in "user_email", :with => "169.healthyeff@gmail.com"
+  fill_in "user_password", :with => "northsidepotato"
+  click_button "Log in"
 end
 
 And /I am on any page/ do
