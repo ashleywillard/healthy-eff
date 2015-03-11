f97bf9f06e6708442ba1bc3ef3fa58c2390ebc1c
Allan Tang (saasbook) <allan_tang@berkeley.edu>
2015-03-10 22:11:14 -0700

    Updated rb files and adding reason column to Day model

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,5 +1,4 @@
 class Activity < ActiveRecord::Base
-  attr_accessible :name, :duration, :approved
-  belongs_to :user # through day?
+  attr_accessible :name, :duration
   belongs_to :day
 end
--- app/models/day.rb
+++ app/models/day.rb
@@ -1,5 +1,5 @@
 class Day < ActiveRecord::Base
   belongs_to :user
   has_many :activities
-  attr_accessible :date, :total_time
+  attr_accessible :date, :total_time, :reason, :approved
 end
