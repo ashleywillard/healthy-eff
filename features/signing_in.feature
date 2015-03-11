7fb8174e0375bb5eab7828d08a89f8afdcd7a38f
Michelle Lin <mj.lin@berkeley.edu>
2015-03-10 21:18:27 -0700

    Passing Cucumber tests for signing in feature

--- features/signing_in.feature
+++ features/signing_in.feature
@@ -5,6 +5,7 @@ Feature: Signing in
   I want to log-in to my account
 
 Background: users in database
+  Given the following users exist:
   | email                       | password              | password_confirmation |    
   | 169.healthyeff@gmail.com    | northsidepotato       | northsidepotato       |
 
@@ -14,15 +15,15 @@ Scenario: Logging in with the correct credentials
   And I press “Log in”
   Then I should see "Signed in successfully."
 
+Scenario: Logging in with incorrect password
+  Given I am on the sign in page
+  When I fill in my username and the wrong password
+  And I press “Log in”
+  Then I should be on the sign in page
+  And I should see "Invalid email or password."
+
 Scenario: Logging out
   Given I am signed in
   And I am on any page
   When I follow "Sign Out"
-  Then I should be on the “Sign In” page
-
-Scenario: Logging in with incorrect password
-  Given I am on the sign in page
-  When I fill in my username and the wrong password
-  And I press “Login”
-  Then I should not be logged in
-  And I should be on the sign in page
+  Then I should be on the sign in page
--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 Given /the following users exist/ do |users_table|
   users_table.hashes.each do |user|
     User.create!(user)
@@ -6,8 +8,8 @@ end
 
 Given /I am signed|logged in/ do
   visit '/users/sign_in'
-  fill_in "user_email", :with => "169.healthyeff@gmail.com"
-  fill_in "user_password", :with => "northsidepotato"
+  fill_in "user_email", :with => '169.healthyeff@gmail.com'
+  fill_in "user_password", :with => 'northsidepotato'
   click_button "Log in"
 end
 
@@ -20,14 +22,23 @@ When /I fill in my username and password/ do
   fill_in "user_password", :with => "northsidepotato"
 end
 
+When /I fill in my username and the wrong password/ do
+  fill_in "user_email", :with => "169.healthyeff@gmail.com"
+  fill_in "user_password", :with => "badpotato"
+end
+
+Then /I should be on the sign in page/ do
+  assert page.current_path == new_user_session_path
+end
+
 And /I am on any page/ do
-	# Undefined for now 
+  # Undefined for now 
 end
 
-When (/^I follow “(.*)”$/) do |link|
-  click_link(link)
-end
-
-When(/^I press “(.*)”$/) do |button|
+When /^I press “(.*)”$/ do |button|
   click_button(button)
+end
+
+When /^(?:|I )follow "([^"]*)"$/ do |link|
+  click_link(link)
 end
