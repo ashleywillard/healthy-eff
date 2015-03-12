84862347d69e5ccc16068711dd7f7e9d0b0f672d
Alex Ho <alexho1928@gmail.com>
2015-03-11 21:46:45 -0700

    cucumber steps implemented without captcha

--- features/step_definitions/log_today_activity_steps.rb
+++ features/step_definitions/log_today_activity_steps.rb
@@ -2,28 +2,28 @@ Given /I am on the home page/ do
   visit '/today'
 end
 
-Given /I am logged in/ do
-  visit '/users/sign_in'
-  fill_in "user_email", :with => "169.healthyeff@gmail.com"
-  fill_in "user_password", :with => "northsidepotato"
-  click_button "Log in"
-end
-
 When /I fill in activity with:(.*)/ do |entry_list|
-  temp = entry_list.split(' ')
+  temp = entry_list.split(',')
   temp.each do |entry|
-    activity = entry.split(',')
+    activity = entry.split(' ')
     
-    fill_in "movie_title", :with => "northsidepotato"
-    #page.find("#movie[title]")
-
-    #set_hidden_field 'activity_name', :to => activity[0]
-    #set_hidden_field 'activity_duration', :to => activity[1]
+    fill_in "activity_name", :with => activity[0]
+    fill_in "activity_duration", :with => activity[1]
   end
 end
 
 When /I write the captcha text in the textbox/ do
-  activities_controller.any_instance.stubs(:check_simple_captcha).returns(true)
+  #activities_controller.any_instance.should_receive(:check_simple_captcha).and_return(true)
 end
 
+Then /I should be on my profile page/ do
+  assert page.current_path == profile_path
+end
 
+Then /^(?:|I )should see "([^"]*)"$/ do |text|
+  if page.respond_to? :should
+    page.should have_content(text)
+  else
+    assert page.has_content?(text)
+  end
+end
