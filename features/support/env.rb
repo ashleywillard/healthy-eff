2c16c98b91153c3b3399269da858afb0cea6f49f
Allan Tang (saasbook) <allan_tang@berkeley.edu>
2015-03-12 16:11:06 -0700

    Simplecov actually covers stuff now

--- features/support/env.rb
+++ features/support/env.rb
@@ -4,9 +4,9 @@
 # instead of editing this one. Cucumber will automatically load all features/**/*.rb
 # files.
 
-require 'cucumber/rails'
 require 'simplecov'
-SimpleCov.start
+SimpleCov.start 'rails'
+require 'cucumber/rails'
 
 # Capybara defaults to CSS3 selectors rather than XPath.
 # If you'd prefer to use XPath, just uncomment this line and adjust any
