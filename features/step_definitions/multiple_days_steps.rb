9a61ec1d14dddc50bf9fed9a490a910269aba76a
Alex Ho <alexho1928@gmail.com>
2015-03-12 21:01:14 -0700

    Updated multiple day/exercise test

--- features/step_definitions/multiple_days_steps.rb
+++ features/step_definitions/multiple_days_steps.rb
@@ -6,10 +6,44 @@ And /I am on the multiple day input page/ do
   assert page.current_path == multiple_days_path
 end
 
-When /I click "Add day"/ do
-  click_link "Add Day"
-end
+When /I fill in day and activity with:(.*)/ do |input|
+  date_entries = input.split('|')
+  date_entries.each do
+    click_link('Add Day')
+  end
+
+  add_activity_ids = page.body.scan(//m) #STILL FILL IN 
+
+  date_entries.zip(add_activity_ids).each do |date_entry, add_act_link|
+    (1..date_entry.split(',').length-1).each do
+      click_link(add_act_link)
+    end
+  end
+
+  date_ids = page.body.scan(/id="user_days_attributes_.{0,20}_date/m)
+  name_ids = page.body.scan(/id="user_days_attributes_.{0,20}_activities_attributes_.{0,20}_name/m)
+  duration_ids = page.body.scan(/id="user_days_attributes_.{0,20}_activities_attributes_.{0,20}_duration/m)
+  exercise_name = Array.new
+  duration_name = Array.new
+
+  date_entries.zip(date_ids) do |date_entry, date_id|
+    
+    act_durs=date_entry.split(',')
+    target_date = act_durs.shift
+    fill_in date_id[4..-1], :with => date_id
+    
+    act_durs.each do |act_dur|
+      fill_in name_ids.shift[4..-1], :with => act_dur.first
+      if act_dur.first != act_dur.last
+        fill_in duration_ids.shift[4..-1], :with => act_dur.last
+      end
+    end
+  end
+
   
+    
+  end
+end  
 
 And /I fill out "Activity"/ do
   print page.html
