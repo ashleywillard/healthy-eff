0f470cf2b178f169bbbd588c278f3592541c0920
Allan Tang (saasbook) <allan_tang@berkeley.edu>
2015-03-12 23:21:14 -0700

    Added restful routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Healthyeff::Application.routes.draw do
   devise_for :users, :skip => [:registrations] 
   as :user do
     get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
-    put 'users' => 'devise/registrations#update', :as => 'user_registration'
+    put 'users/edit' => 'devise/registrations#update', :as => 'user_registration'
   end
 
   #required for devise to work, redirects to path below after user logs in
