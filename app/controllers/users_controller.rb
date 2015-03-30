5cc41b5437a14984fe1ef6904d180e66af390712
Alex Ho <alexho1928@gmail.com>
2015-03-30 15:02:32 -0700

    getting workout days for last 2 months

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,12 @@
 class UsersController < ApplicationController
 
   def profile
-  	@now = Time.now
-  	@currentMonth = @now.month
+  	@now = DateTime.now
+  	@currentMonth = @now.strftime('%m').to_i
   	@previousMonth = @currentMonth-1 == 0 ? 12 : @currentMonth-1 
-    
+    #@calendarInfo=User.Month.Day.Activity.where(:user_id => current_user,
+    											#:month => @currentMonth,
+    											#:year => @now.year)
   end
-
-  def invite_user
-    @user = User.invite!(:email => params[:user][:email], :name => params[:user][:name])
-    render :json => @user
-  end
-
 
 end
