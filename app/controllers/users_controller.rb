e1e8f45db613283c4f81674ada42b7f6141cee4e
Alex Ho <alexho1928@gmail.com>
2015-03-30 14:33:49 -0700

    boom

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
 
   def profile
+  	@now = Time.now
+  	@currentMonth = @now.month
+  	@previousMonth = @currentMonth-1 == 0 ? 12 : @currentMonth-1 
     
   end
 
