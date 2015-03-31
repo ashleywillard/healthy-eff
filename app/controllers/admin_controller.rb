72d78f4e913c613fe363296decf13a4757853e08
Michelle Lin <mj.lin@berkeley.edu>
2015-03-30 20:37:37 -0700

    Placeholder comments for admin logic

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,14 @@
+class AdminController < ApplicationController
+
+  # either copy-paste the before_filter from ActivitiesController here to ensure
+  # admin is logged in before executing any admin-related logic, or move the
+  # before_filter function to ApplicationController, since you have to be logged
+  # in to do basically anything for our app?
+
+  # function to generate list of employees for the list view
+
+  # function to generate list of multiple-day activities for to-be-approved view
+
+  # potentially ashley/allan's stuff for adding and removing employees
+
+end
