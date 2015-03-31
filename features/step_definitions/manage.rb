adc118483419d953d1ece4211336ab402f1e53a6
Ashley <ashleywillard55@gmail.com>
2015-03-30 17:34:37 -0700

    cucumber stuff

---
+++ features/step_definitions/manage.rb
@@ -0,0 +1,15 @@
+# encoding: UTF-8
+
+Given /I am an admin/ do
+  visit '/users/sign_in'
+  fill_in "user_email", :with => '169.healthyeff@gmail.com'
+  fill_in "user_password", :with => 'northsidepotato'
+  click_button "Log in"
+end
+
+Given /I am a nonadmin/ do
+  visit '/users/sign_in'
+  fill_in "user_email", :with => 'healthypotato@gmail.com'
+  fill_in "user_password", :with => 'hotpotato'
+  click_button "Log in"
+end
