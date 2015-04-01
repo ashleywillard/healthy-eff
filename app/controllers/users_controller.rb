ab07c007c833cf6e4c2d15afab161fb83543bc33
Alex Ho <alexho1928@gmail.com>
2015-03-31 21:20:37 -0700

    implementing displaying old workouts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,17 +3,29 @@ class UsersController < ApplicationController
   def profile
   	@now = DateTime.now
   	@currentMonth = @now.strftime('%m').to_i
-  	@previousMonth = @currentMonth-1 == 0 ? 12 : @currentMonth-1 
-    @user_month = Month.where(:month => month_var, 
-    							:year => year_var, 
-    							:user_id => current_user.id)
+    @currentYear = @now.strftime('%Y').to_i
 
-    @events = []
-    def populate_events 
-	    user_month.days each |day| do
-	    	day.activities each |day| do
-	    		events.add(day.date, activity.activity, )	
+    @previousMonth = @currentMonth - 1
+    @previousYear = @currentYear
+    if @currentMonth== 0
+      @previousMonth = 12
+      @previousYear -= 1
     end
+
+    @curr_user_month = Month.where(:month => @currentMonth, 
+    							                 :year => @currentMonth, 
+    							                 :user_id => current_user.id)
+    @prev_user_month = Month.where(:month => @previousMonth, 
+    							                 :year => @previousYear, 
+    							                 :user_id => current_user.id)	
   end
 
+   #  @workouts = []
+   #  def populate_events 
+	  #   @curr_user_month.days each |day| do
+	  #   	day.activities each |day| do
+	  #   		workouts.add(day.date, activity.activity)	
+   #  		end
+  	# 	end
+  	# end
 end
