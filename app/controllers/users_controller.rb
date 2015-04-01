37181631f2a84d4160ddc7fc1f964aabc0eba560
cschafle <chlorrinne@gmail.com>
2015-04-01 00:32:19 -0300

    populate events from database

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,16 @@ class UsersController < ApplicationController
   	@now = DateTime.now
   	@currentMonth = @now.strftime('%m').to_i
   	@previousMonth = @currentMonth-1 == 0 ? 12 : @currentMonth-1 
-    #@calendarInfo=User.Month.Day.Activity.where(:user_id => current_user,
-    											#:month => @currentMonth,
-    											#:year => @now.year)
+    @user_month = Month.where(:month => month_var, 
+    							:year => year_var, 
+    							:user_id => current_user.id)
+
+    @events = []
+    def populate_events 
+	    user_month.days each |day| do
+	    	day.activities each |day| do
+	    		events.add(day.date, activity.activity, )	
+    end
   end
 
 end
