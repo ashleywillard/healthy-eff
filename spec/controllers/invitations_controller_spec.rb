2c291eeb5f8f5a4ea20fd9f79657bd50a2206094
Allan Tang (saasbook) <allan_tang@berkeley.edu>
2015-04-02 15:04:52 -0700

    Starting rpsec for invitation controller

--- spec/controllers/invitations_controller_spec.rb
+++ spec/controllers/invitations_controller_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe InvitationsController, :type => :controller do
 
+  describe 'non-admin' do
+    it 'should not be able to send invitations' do
+      user = mock('User')
+      user.admin = false
+      user.stub!(:password)
+      user.stub!(:email)
+      user.save!
+      allow(request.env['warden']).to receive(:authenticate!).and_return(user)
+      allow(controller).to receive(:current_user).and_return(user)
+      post :user_invitation
+      expect(response).to redirect_to(root_path)
+      flash[:notice].should eql('You are not authorized to send invites!')
+    end
+  end
 end
