77d64ee47d198b67de1f5f5634a17eb0e06be4c0
Michelle Lin <mj.lin@berkeley.edu>
2015-04-02 19:08:01 -0700

    Added steps for creating pending activities

--- features/step_definitions/create_steps.rb
+++ features/step_definitions/create_steps.rb
@@ -13,3 +13,19 @@ Given (/the following admins exist/) do |users_table|
     user.save
   end
 end
+
+Given (/(.*) (?:|pending|unapproved) (?:|activities|days) exist/) do |num|
+  if num.casecmp("No") ; pass ; end
+  u = User.create! :email => "blah@blah.com",
+                   :password => "asdfjkl;asdfjkl;",
+                   :password_confirmation => "asdfjkl;asdfjkl;"
+  m = Month.create! :user_id => u.id
+  num.to_i.times do
+    Day.create! :date => Time.strptime("04/01/2015", "%m/%d/%Y"),
+                :approved => false,
+                :denied => false,
+                :total_time => 60,
+                :reason => 'Reason',
+                :month_id => m.id
+  end
+end
