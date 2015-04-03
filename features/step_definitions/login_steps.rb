37d497ee7286ecddf3f5ecebaaa1c1f6f559f3c2
Michelle Lin <mj.lin@berkeley.edu>
2015-04-02 17:17:07 -0700

    Moved table creation definitions to a new file

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -1,19 +1,5 @@
 # encoding: UTF-8
 
-Given /the following users exist/ do |users_table|
-  users_table.hashes.each do |user|
-    User.create!(user)
-  end
-end
-
-Given /the following admins exist/ do |users_table|
-  users_table.hashes.each do |user|
-    user = User.create!(user)
-    user.admin = true
-    user.save
-  end
-end
-
 Given /I am signed|logged in as an admin/ do
   visit '/users/sign_in'
   fill_in "user_email", :with => '169.healthyeff@gmail.com'
