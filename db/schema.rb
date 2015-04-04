bcbb734a18c0d62b2ab60631287c570b1097b983
Michelle Lin <mj.lin@berkeley.edu>
2015-04-03 18:42:56 -0700

    Dropped and re-added admin column from Users table, since PostGreSQL doesn't support change_columns

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150402221350) do
+ActiveRecord::Schema.define(:version => 20150404014059) do
 
   create_table "activities", :force => true do |t|
     t.integer  "duration"
@@ -66,7 +66,6 @@ ActiveRecord::Schema.define(:version => 20150402221350) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
-    t.boolean  "admin"
     t.string   "invitation_token"
     t.datetime "invitation_created_at"
     t.datetime "invitation_sent_at"
@@ -77,6 +76,7 @@ ActiveRecord::Schema.define(:version => 20150402221350) do
     t.integer  "invitations_count",      :default => 0
     t.string   "first_name"
     t.string   "last_name"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
