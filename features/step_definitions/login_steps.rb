0aee620fc3f6a8542abcd9405ebebf1a0859fbea
Allan Tang (saasbook) <allan_tang@berkeley.edu>
2015-04-13 15:42:11 -0700

    Changed Log In to Sign In. For some reason it changed...

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -1,18 +1,17 @@
 # encoding: UTF-8
 
 Given /I am signed|logged in as an admin/ do
-  save_and_open_page
   visit '/users/sign_in'
   fill_in "user_email", :with => '169.healthyeff@gmail.com'
   fill_in "user_password", :with => '?Northsidepotato169'
-  click_button "Log in"
+  click_button "Sign In"
 end
 
 Given /I am signed|logged in as a non-admin/ do
   visit '/users/sign_in'
   fill_in "user_email", :with => 'healthypotato@gmail.com'
   fill_in "user_password", :with => '?Hotpotato169'
-  click_button "Log in"
+  click_button "Sign In"
 end
 
 When /I fill in my username and password/ do
