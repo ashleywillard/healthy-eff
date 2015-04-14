4f29cf5419abaf701137d8d082fad13e2ea1dcb8
Michelle Lin <mj.lin@berkeley.edu>
2015-04-14 01:49:34 -0700

    Refactored accounting

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -5,19 +5,8 @@ class AdminController < ApplicationController
   # Admin list view
   def index
     session[:months_ago] ||= 0
-#     @months_ago = session[:months_ago]
-#     if not params[:navigate].nil?
-#       @months_ago += 1 if params[:navigate] == "Previous"
-#       @months_ago -=1 if params[:navigate] == "Next"
-#       session[:months_ago] = @months_ago
-#     end
     session[:months_ago] += 1 if params[:navigate] == "Previous"
     session[:months_ago] -= 1 if params[:navigate] == "Next"
-#     d = @months_ago.to_i.months.ago
-#     @month = d.strftime("%B")
-#     @year = d.strftime("%Y")
-#     @month_num = d.strftime("%m")
-#     @user_months = Month.where(:month => @month_num, :year => @year)
     @date = session[:months_ago].to_i.months.ago
     @user_months = Month.where(:month => @date.strftime("%m"),
                                :year => @date.strftime("%Y"))
@@ -52,14 +41,12 @@ class AdminController < ApplicationController
 
   # Generate PDF for individual accounting sheet
   def accounting
-    id = params[:id] ; u = User.find_by_id(id)
-    @first_name = u.first_name ; @last_name = u.last_name
-
-    @date = Date.today # CHANGE TO REFLECT MONTH CURRENTLY BEING VIEWED
+    @user = User.find_by_id(params[:id])
+    @date = session[:months_ago].to_i.months.ago # CHANGE TO REFLECT MONTH CURRENTLY BEING VIEWED
     @num_days = Time.days_in_month(@date.month, @date.year) + 1 # +1 here as test for 31 days
     records = Month.where(:month => @date.strftime("%m"),
                           :year => @date.strftime("%Y"),
-                          :user_id => id).first
+                          :user_id => params[:id]).first
     if records.nil?
       flash[:notice] = "No recorded activities for #{@first_name} #{@last_name} for #{@date.strftime("%B")} #{@date.strftime("%Y")}."
       redirect_to admin_list_path and return
@@ -67,11 +54,6 @@ class AdminController < ApplicationController
       @user_days = records.num_of_days
     end
     generate_pdf("accounting", "acct-#{@last_name}-#{@date.strftime("%B")}-#{@date.strftime("%Y")}.pdf")
-#     html = render_to_string(:layout => false, :action => "accounting.html.haml")
-#     kit = PDFKit.new(html)
-#     send_data(kit.to_pdf, :filename => "acct-#{@last_name}-#{@date.strftime("%B")}-#{@date.strftime("%Y")}.pdf",
-#                           :type => 'application/pdf',
-#                           :disposition => "inline")
   end
 
   # Generate PDF for all employees this month (audit sheet)
@@ -81,11 +63,6 @@ class AdminController < ApplicationController
     @year = @date.strftime("%Y")
     @user_months = Month.where(:month => @month_num, :year => @year)
     generate_pdf("audit", "audit-#{@date.strftime("%B")}-#{@date.strftime("%Y")}.pdf")
-#     html = render_to_string(:layout => false, :action => "audit.html.haml")
-#     kit = PDFKit.new(html, :orientation => 'Landscape')
-#     send_data(kit.to_pdf, :filename => "audit-#{@date.strftime("%B")}-#{@date.strftime("%Y")}.pdf",
-#                           :type => 'application/pdf',
-#                           :disposition => "inline")
   end
 
   def generate_pdf(type, name)
@@ -111,8 +88,5 @@ class AdminController < ApplicationController
       redirect_to today_path
     end
   end
-
-  # potentially ashley/allan's stuff for adding and removing employees?
-  # might also be in UsersController, depending on their implementation
 
 end
