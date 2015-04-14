116ca85eb0f9969bb0a11c1bae86c729de03ea4b
Michelle Lin <mj.lin@berkeley.edu>
2015-04-13 19:42:18 -0700

    Checking new password field with 'blank' instead of 'il' solves the filtered password problem

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,11 +1,17 @@
 class RegistrationsController < Devise::RegistrationsController
 
   def update
-    if not params[:user][:password].nil?
-      current_user.password_changed = true
-      current_user.save
+    if not current_user.password_changed?
+      if new_password_provided?
+        current_user.password_changed = true
+        current_user.save
+      end
     end
     super
+  end
+
+  def new_password_provided?
+    not params[:user][:password].blank?
   end
 
 end
