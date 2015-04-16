ba209eb7b17901b8800a05723a6968d7abd1163c
Michelle Lin <mj.lin@berkeley.edu>
2015-04-16 16:13:46 -0700

    Refactoring and cleanup

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -8,9 +8,10 @@ class AdminController < ApplicationController
     session[:sort] = params[:sort] if not params[:sort].nil?
     navigate_months()
     @date = get_date()
-    @user_months = Month.get_user_months(get_month(@date), get_year(@date))
-    @user_months = sort_admin_list(@user_months) unless session[:sort].nil?
-    @user_months = @user_months.reject{ |x| x.get_num_approved_days() == 0 } unless @user_months.nil?
+    @user_months = sort(Month.get_user_months(get_month(@date), get_year(@date)))
+    unless @user_months.nil?
+      @user_months = @user_months.reject{ |x| x.get_num_approved_days() == 0 }
+    end
   end
 
   # :get for pending activities
@@ -47,34 +48,24 @@ class AdminController < ApplicationController
     @date = get_date()
     @num_days = Time.days_in_month(@date.month, @date.year)
     @records = Month.get_month_model(params[:id], @date.month, @date.year)
-    if @records.nil? or @records.get_num_approved_days() == 0
-      handle_no_records()
-    else
-      @user_days = @records.get_num_approved_days()
-      generate_pdf("accounting", "acct-#{@user.last_name}-#{get_month_name(@date)}-#{get_year(@date)}.pdf")
-    end
+    handle_no_records() if @records.nil? or @records.get_num_approved_days() == 0
+    @user_days = @records.get_num_approved_days()
+    html = render_to_string(:layout => false, :action => 'accounting.html.haml')
+    generate_pdf("accounting", "acct-#{@user.last_name}-#{get_month_name(@date)}-#{get_year(@date)}.pdf", html)
   end
 
   def group_accounting
-    html = ""
+    redirect_to admin_list_path and return if params[:selected].nil?
     @date = get_date()
     @num_days = Time.days_in_month(@date.month, @date.year)
-    redirect_to admin_list_path and return if params[:selected].nil?
+    html = ""
     params[:selected].each do |m_id|
       if m_id == params[:selected][0] ; @first = true ; else ; @first = false ; end
       @user = User.find_by_id(Month.find_by_id(m_id).user.id)
       @records = Month.get_month_model(@user.id, @date.month, @date.year)
-      if @records.nil?
-        @user_days = 0
-      else
-        @user_days = @records.get_num_approved_days()
-      end
+      @records.nil? ? @user_days = 0 : @user_days = @records.get_num_approved_days()
       html << render_to_string(:layout => false, :action => 'accounting.html.haml')
-      kit = PDFKit.new(html)
-      kit.stylesheets << "#{Rails.root}/app/assets/stylesheets/pdf.css"
-      send_data(kit.to_pdf, :filename => "acct-#{get_month_name(@date)}-#{get_year(@date)}.pdf",
-                            :type => 'application/pdf',
-                            :disposition => "inline")
+      generate_pdf("accounting", "acct-#{get_month_name(@date)}-#{get_year(@date)}.pdf", html)
     end
   end
 
@@ -83,19 +74,17 @@ class AdminController < ApplicationController
     @date = session[:months_ago].to_i.months.ago
     @user_months = Month.find(:all, :conditions => {:month => get_month(@date), :year => get_year(@date)},
                               :joins => :user, :order => 'users.last_name')
-    generate_pdf("audit", "audit-#{get_month_name(@date)}-#{get_year(@date)}.pdf")
+    html = render_to_string(:layout => false, :action => 'audit.html.haml')
+    generate_pdf("audit", "audit-#{get_month_name(@date)}-#{get_year(@date)}.pdf", html)
   end
 
-  def generate_pdf(type, name)
+  def generate_pdf(type, name, html)
     case type
       when 'audit'
-        action = 'audit.html.haml'
         orientation = 'Landscape'
       when 'accounting'
-        action = 'accounting.html.haml'
         orientation = 'Portrait'
     end
-    html = render_to_string(:layout => false, :action => action)
     kit = PDFKit.new(html, :orientation => orientation)
     kit.stylesheets << "#{Rails.root}/app/assets/stylesheets/pdf.css"
     send_data(kit.to_pdf, :filename => name,
@@ -110,12 +99,12 @@ class AdminController < ApplicationController
   end
 
   def sort_admin_list(list)
-    list.joins(:user).order("users." + session[:sort])
+    session[:sort].nil? ? list : list.joins(:user).order("users." + session[:sort])
   end
 
   def handle_no_records
     flash[:notice] = "No recorded activities for #{@user.first_name} #{@user.last_name} for #{@date.strftime("%B")} #{@date.strftime("%Y")}."
-    redirect_to admin_list_path
+    redirect_to admin_list_path and return
   end
 
   def get_date
